481e4e64c804d5873a250c4871e56e471c3d0989
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-15 15:59:11 +0100

    ADD customizable input for the functions

--- people.js
+++ people.js
@@ -1,9 +1,11 @@
 const person = require('./names.js')
 const hobbiesList = require('./hobbies.js')
 
+const params = process.argv.slice(2)
+
 function people(){
-    const {firstName , lastName} = person('Simone' , 'Domenici')
-    const {hobbies} = hobbiesList('Calcio' , 'Videogiochi' , 'Anime')
+    const {firstName , lastName} = person(params[0] ?? 'Simone' ,params[1] ?? 'Domenici')
+    const {hobbies} = hobbiesList(params[2] ?? 'Calcio' ,params[3] ?? 'Videogiochi' ,params[4] ?? 'Anime')
     return {
         firstName,
         lastName,
